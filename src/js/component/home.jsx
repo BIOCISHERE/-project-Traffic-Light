bb267ce1d3118e36a542343c2a93578ec252b6e8
BIOCISHERE <bioc.is.here.8@gmail.com>
2023-03-15 22:58:45 +0000

    basic traficligth done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [color, setColor] = useState("");
+
+  return (
+    <>
+      <div className="sign">
+        <div className="stick"></div>
+        <div className="traficLigth">
+          <div onClick={() => setColor("red")} className={(color === "red") ? "ligth red my-2 glowred" : "ligth red my-2"}></div>
+          <div onClick={() => setColor("yellow")} className={(color === "yellow" ? "ligth yellow my-2 glowyellow" : "ligth yellow my-2 ")}></div>
+          <div onClick={() => setColor("green")} className={(color === "green") ? "ligth green my-2 glowgreen" : "ligth green my-2"}></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
