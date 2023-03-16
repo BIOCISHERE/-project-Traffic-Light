cabcaa95203b45a31fc6a70c4736e0aea0d67ca1
BIOCISHERE <bioc.is.here.8@gmail.com>
2023-03-16 01:04:43 +0000

    hecho 2.0

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,6 +7,22 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
   const [color, setColor] = useState("");
   const [wantPurple, setWantPurple] = useState(true)
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
+  const handleClick = () => {
+    setInterval(() => {
+      setColor('red');
+    }, 1000);
+	setInterval(() => {
+		setColor('yellow');
+	}, 2000);
+	setInterval(() => {
+		setColor('green');
+	}, 3000);
+  }
   
 
   return wantPurple ? (
@@ -17,7 +33,9 @@ const Home = () => {
             <div onClick={() => setColor("yellow")} className={(color === "yellow" ? "ligth yellow my-2 glowyellow" : "ligth yellow my-2 ")}></div>
             <div onClick={() => setColor("green")} className={(color === "green") ? "ligth green my-2 glowgreen" : "ligth green my-2"}></div>
         </div> <br />
-		<button type="button" className="btn btn-primary" onClick={() => (setWantPurple(false))  (setColor(''))}>Click to show purple light</button>
+		<button type="button" className="btn btn-primary" onClick={() => setWantPurple(false)}>Click to show purple light</button>
+		<button type="button" className="btn btn-success mx-1" onClick={handleClick}>test ligths</button>
+		<button type="button" className="btn btn-secondary my-1" onClick={refreshPage}>click to refresh page(stop test)</button>
     </div>
   ) : (<div className="sign">
   <div className="stick"></div>
@@ -28,6 +46,8 @@ const Home = () => {
 	  <div onClick={() => setColor("purple")} className={(color === "purple") ? "ligth purple my-2 glowpurple" : "ligth purple my-2"}></div>
   </div> <br />
   <button type="button" className="btn btn-warning" onClick={() => setWantPurple(true)}>Click to hide purple light</button>
+  <button type="button" className="btn btn-success mx-1" onClick={handleClick}>test ligths</button>
+  <button type="button" className="btn btn-secondary my-1" onClick={refreshPage}>click to refresh page(stop test)</button>
 </div>);
     
 };
