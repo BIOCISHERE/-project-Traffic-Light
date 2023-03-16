a8267b1ad83d8743db0616dd6d16a5bc3f284f9b
BIOCISHERE <bioc.is.here.8@gmail.com>
2023-03-16 01:14:02 +0000

    hecho 3.0

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -27,28 +27,31 @@ const Home = () => {
 
   return wantPurple ? (
 	<div className="sign">
-        <div className="stick mt-3"></div>
-        <div className="traficLigth">
-            <div onClick={() => setColor("red")} className={(color === "red") ? "ligth red my-2 glowred" : "ligth red my-2"}></div>
-            <div onClick={() => setColor("yellow")} className={(color === "yellow" ? "ligth yellow my-2 glowyellow" : "ligth yellow my-2 ")}></div>
-            <div onClick={() => setColor("green")} className={(color === "green") ? "ligth green my-2 glowgreen" : "ligth green my-2"}></div>
-        </div> <br />
+    <div className="stick mt-3"></div>
+    <div className="traficLigth">
+      <div onClick={() => setColor("red")} className={(color === "red") ? "ligth red my-2 glowred" : "ligth red my-2"}></div>
+      <div onClick={() => setColor("yellow")} className={(color === "yellow" ? "ligth yellow my-2 glowyellow" : "ligth yellow my-2 ")}></div>
+      <div onClick={() => setColor("green")} className={(color === "green") ? "ligth green my-2 glowgreen" : "ligth green my-2"}></div>
+    </div> <br />
+    <h2>Click on a ligth to turn it on!!!</h2>
 		<button type="button" className="btn btn-primary" onClick={() => setWantPurple(false)}>Click to show purple light</button>
 		<button type="button" className="btn btn-success mx-1" onClick={handleClick}>test ligths</button>
 		<button type="button" className="btn btn-secondary my-1" onClick={refreshPage}>click to refresh page(stop test)</button>
-    </div>
-  ) : (<div className="sign">
-  <div className="stick"></div>
-  <div className="traficLigth">
-	  <div onClick={() => setColor("red")} className={(color === "red") ? "ligth red my-2 glowred" : "ligth red my-2"}></div>
-	  <div onClick={() => setColor("yellow")} className={(color === "yellow" ? "ligth yellow my-2 glowyellow" : "ligth yellow my-2 ")}></div>
-	  <div onClick={() => setColor("green")} className={(color === "green") ? "ligth green my-2 glowgreen" : "ligth green my-2"}></div>
-	  <div onClick={() => setColor("purple")} className={(color === "purple") ? "ligth purple my-2 glowpurple" : "ligth purple my-2"}></div>
-  </div> <br />
-  <button type="button" className="btn btn-warning" onClick={() => setWantPurple(true)}>Click to hide purple light</button>
-  <button type="button" className="btn btn-success mx-1" onClick={handleClick}>test ligths</button>
-  <button type="button" className="btn btn-secondary my-1" onClick={refreshPage}>click to refresh page(stop test)</button>
-</div>);
+  </div>
+  ) : (
+  <div className="sign">
+    <div className="stick"></div>
+    <div className="traficLigth">
+	    <div onClick={() => setColor("red")} className={(color === "red") ? "ligth red my-2 glowred" : "ligth red my-2"}></div>
+	    <div onClick={() => setColor("yellow")} className={(color === "yellow" ? "ligth yellow my-2 glowyellow" : "ligth yellow my-2 ")}></div>
+	    <div onClick={() => setColor("green")} className={(color === "green") ? "ligth green my-2 glowgreen" : "ligth green my-2"}></div>
+	    <div onClick={() => setColor("purple")} className={(color === "purple") ? "ligth purple my-2 glowpurple" : "ligth purple my-2"}></div>
+    </div> <br />
+    <h2>Click on a ligth to turn it on!!!</h2>
+    <button type="button" className="btn btn-warning" onClick={() => setWantPurple(true)}>Click to hide purple light</button>
+    <button type="button" className="btn btn-success mx-1" onClick={handleClick}>test ligths</button>
+    <button type="button" className="btn btn-secondary my-1" onClick={refreshPage}>click to refresh page(stop test)</button>
+  </div>);
     
 };
 
